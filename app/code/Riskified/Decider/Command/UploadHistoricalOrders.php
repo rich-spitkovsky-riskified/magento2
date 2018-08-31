5755c2e9523e3aea4c2a3360df717206c5898d0e
Wojciech Kamiski <wojtek.kaminski@gmail.com>
2018-08-31 13:16:03 +0200

    Added timeframes for historical orders call

--- app/code/Riskified/Decider/Command/UploadHistoricalOrders.php
+++ app/code/Riskified/Decider/Command/UploadHistoricalOrders.php
@@ -2,17 +2,18 @@
 namespace Riskified\Decider\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Riskified\Common\Riskified;
-use Riskified\Common\Env;
 use Riskified\Common\Validations;
 use Riskified\Common\Signature;
 use Riskified\OrderWebhook\Model;
-use Riskified\OrderWebhook\Transport;
 use Riskified\OrderWebhook\Transport\CurlTransport;
+
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
 
 class UploadHistoricalOrders extends Command
 {
@@ -25,21 +26,28 @@ class UploadHistoricalOrders extends Command
     protected $_currentPage = 1;
     protected $_orders;
 
+    private $startDateFilter = null;
+    private $endDateFilter = null;
+    private $filterBuilder;
+
     const BATCH_SIZE = 10;
+    const START_DATE = 'startdate';
+    const END_DATE = 'enddate';
 
     public function __construct(
         \Magento\Framework\App\State $state,
         \Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
-        \Magento\Framework\Api\SearchCriteria $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder
     ) {
         $state->setAreaCode('adminhtml');
 
         $this->_scopeConfig             = $scopeConfig;
         $this->_orderRepository         = $orderRepository;
         $this->_searchCriteriaBuilder   = $searchCriteriaBuilder;
-
+        $this->filterBuilder = $filterBuilder;
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $this->_orderHelper = $objectManager->get('\Riskified\Decider\Api\Order\Helper');
 
@@ -54,8 +62,24 @@ class UploadHistoricalOrders extends Command
      */
     protected function configure()
     {
+        $options = [
+            new InputOption(
+                self::START_DATE,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Start Date'
+            ),
+            new InputOption(
+                self::END_DATE,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'END Date'
+            ),
+        ];
+
         $this->setName('riskified:sync:historical-orders');
-        $this->setDescription('Send your historical orders to riskified backed');
+        $this->setDescription('Send your orders to Riskified.');
+        $this->setDefinition($options);
 
         parent::configure();
     }
@@ -70,29 +94,30 @@ class UploadHistoricalOrders extends Command
         $env = constant('\Riskified\Common\Env::' . $this->_scopeConfig->getValue('riskified/riskified/env'));
         $domain = $this->_scopeConfig->getValue('riskified/riskified/domain');
 
-        $output->writeln("Riskified auth token: $authToken \n");
-        $output->writeln("Riskified shop domain: $domain \n");
-        $output->writeln("Riskified target environment: $env \n");
-        $output->writeln("*********** \n");
-
+        $output->writeln(sprintf("Riskified auth token: %s", $authToken));
+        $output->writeln(sprintf("Riskified shop domain: %s", $domain));
+        $output->writeln(sprintf("Riskified target environment: %s", $env));
+        $output->writeln("***********");
 
         Riskified::init($domain, $authToken, $env, Validations::SKIP);
 
-        $fullOrderRepository = $this->getEntireCollection();
-        $total_count = $fullOrderRepository->getSize();
+        $this->startDateFilter = $input->getOption("startdate");
+        $this->endDateFilter = $input->getOption("enddate");
 
-        $output->writeln("Starting to upload orders, total_count: $total_count \n");
+        $fullOrderRepository = $this->getEntireCollection();
+        $totalCount = $fullOrderRepository->getSize();
+
+        $output->writeln(sprintf("Starting to upload orders, total_count: %s \n", $totalCount));
+
         $this->getCollection();
-        while ($this->_totalUploaded < $total_count) {
+
+        while ($this->_totalUploaded < $totalCount) {
             try {
                 $this->postOrders();
                 $this->_totalUploaded += count($this->_orders);
                 $this->_currentPage++;
-                $output->writeln("Uploaded " .
-                    $this->_totalUploaded .
-                    " of " .
-                    $total_count
-                    ." orders\n");
+
+                $output->writeln(sprintf("Uploaded %s of %s orders\n", $this->_totalUploaded, $totalCount));
 
                 $this->getCollection();
             } catch (\Exception $e) {
@@ -105,26 +130,60 @@ class UploadHistoricalOrders extends Command
     /**
      * Retrieve prepared order collection for counting values
      *
-     * @return \Magento\Sales\Api\Data\OrderSearchResultInterface
+     * @return \Magento\Sales\Model\ResourceModel\Order\Collection
      */
-    protected function getEntireCollection() {
+    protected function getEntireCollection()
+    {
+        if ($this->startDateFilter) {
+            try {
+                $startDate = new \DateTime($this->startDateFilter);
+                $filterStartDate = $this->filterBuilder
+                    ->setField("main_table.created_at")
+                    ->setValue($startDate->format("Y-m-d 00:00:00"))
+                    ->setConditionType("gteq")
+                    ->create();
+                $this->_searchCriteriaBuilder->addFilter($filterStartDate);
+            } catch(\Exception $e) {}
+        }
+        if ($this->endDateFilter) {
+            try {
+                $startDate = new \DateTime($this->endDateFilter);
+                $filterStartDate = $this->filterBuilder
+                    ->setField("main_table.created_at")
+                    ->setValue($startDate->format("Y-m-d 23:59:59"))
+                    ->setConditionType("lteq")
+                    ->create();
+                $this->_searchCriteriaBuilder->addFilter($filterStartDate);
+            } catch(\Exception $e) {}
+        }
+
+        $searchCriteria = $this->_searchCriteriaBuilder->create();
+
         $orderResult = $this
             ->_orderRepository
-            ->getList($this->_searchCriteriaBuilder);
+            ->getList($searchCriteria);
+
         return $orderResult;
     }
 
     /**
      * Retrieve paginated collection
      *
-     * @return void
+     * @return \Magento\Sales\Api\Data\OrderInterface[]
      */
-    protected function getCollection() {
+    protected function getCollection()
+    {
         $this->_searchCriteriaBuilder
             ->setPageSize(self::BATCH_SIZE)
             ->setCurrentPage($this->_currentPage);
-        $orderResult = $this->_orderRepository->getList($this->_searchCriteriaBuilder);
+
+        $searchCriteria = $this->_searchCriteriaBuilder->create();
+
+        $orderResult = $this->_orderRepository->getList($searchCriteria);
+
         $this->_orders = $orderResult->getItems();
+
+        return $this->_orders;
     }
 
     /**
